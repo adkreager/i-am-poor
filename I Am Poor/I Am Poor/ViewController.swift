0e0680316dc112b0aecfe84debc4fcc10eabc48d
Alexis <alexisdkreager@gmail.com>
2022-03-25 10:33:44 -0500

    Add files via upload

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by alexis kreager on 3/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
